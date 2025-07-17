63307481cf1b86930a695d18811cd9341c24b68c
Mug56-sys <kacper.bandula29@gmail.com>
2025-07-17 21:37:37 +0200

    Hidden img

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,18 @@ export type memoType={
    },
    {id:4,
     img:"https://images.unsplash.com/photo-1442458017215-285b83f65851",
-   },]
+   },{id:1,
+    img:"https://images.unsplash.com/photo-1626808642875-0aa545482dfb",
+   },
+   {id:2,
+    img:"https://images.unsplash.com/photo-1546842931-886c185b4c8c",
+   },
+   {id:3,
+    img:"https://images.unsplash.com/photo-1520763185298-1b434c919102",
+   },
+   {id:4,
+    img:"https://images.unsplash.com/photo-1442458017215-285b83f65851",
+   }]
    
 
 function App() {
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 
 export default function Card({image}:{image:string}) {
-  console.log(image)
+  const [isHidden,setIsHidden]=useState(true)
+  console.log(isHidden)
   return (
-    <div className='p-20 bg-gray-500 cursor-pointer relative'>
-      <img src={image} className=" absolute inset-0 w-full h-full object-cover hidden"/>
+    <div className='p-20 bg-gray-500 cursor-pointer relative' onClick={()=>setIsHidden(!isHidden)}>
+      <img src={image} className={`absolute inset-0 w-full h-full object-cover ${isHidden ? `hidden`:null}`}/>
     </div>
   )
 }
